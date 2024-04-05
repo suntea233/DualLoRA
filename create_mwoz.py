21da489628412de6e2e9dcf42dc657b00ccf5456
Sun Tea <59692513+suntea233@users.noreply.github.com>
2024-04-05 10:55:59 +0800

    Update create_mwoz.py

--- create_mwoz.py
+++ create_mwoz.py
@@ -28,7 +28,7 @@ DICT_SIZE = 400
 MAX_LENGTH = 50
 IGNORE_KEYS_IN_GOAL = ['eod', 'topic', 'messageLen', 'message']
 
-fin = open('src/utils/mapping.pair','r')
+fin = open('utils/mapping.pair','r')
 replacements = []
 for line in fin.readlines():
     tok_from, tok_to = line.replace('\n', '').split('\t')
